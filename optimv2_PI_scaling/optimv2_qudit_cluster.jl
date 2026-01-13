a429e7d8947bab35a47e81ea1694ec9af30c8dea
Liam Bond <lblb98.lb98@gmail.com>
2026-01-13 16:24:51 +0100

    remove loop, save optim out info

--- optimv2_PI_scaling/optimv2_qudit_cluster.jl
+++ optimv2_PI_scaling/optimv2_qudit_cluster.jl
@@ -37,7 +37,6 @@ end;
 
 heaviside(x) = x ≥ 0 ? 1 : 0;
 checknonneg(λ) = all(x -> x ≥ 0, λ);
-#partbinom(n,λ) = (checknonneg(λ) == 1) ? (factorial(n)/prod(factorial(λ[i]) for i in eachindex(λ))) : 0;
 logpartbinom(n,λ) = (checknonneg(λ) == 1) ? (logfactorial(n) - sum(logfactorial(λ[i]) for i in eachindex(λ))) : 0;
 
 function rules1(n,q,d,codewords)
@@ -282,30 +281,28 @@ res_loop_minimum = []; res_loop_minimizer = []; thread_arr = [];
 
 println("start optimization.")
 
-for i in 1:1
+x0 = normalize(rand(num_var_params)) # this is for real 
+# x0 = normalize(rand(ComplexF64,num_var_params)) # for Vlad: this is complex 
 
-    x0 = normalize(rand(num_var_params)) # this is for real 
-    # x0 = normalize(rand(ComplexF64,num_var_params)) # for Vlad: this is complex 
+res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), autodiff = AutoReverseDiff(),
+# res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), 
+            Optim.Options(iterations=100000,
+                        g_tol=1e-30,
+                        f_abstol=1e-30,
+                        f_reltol=1e-30,
+                        allow_f_increases=true,
+                        show_trace=false,
+                        callback=callback,
+                        time_limit = 60 * 60 * 24) # put a time limit of 1 day (units of seconds))
+                )
 
-    res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), autodiff = AutoReverseDiff(),
-    # res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), 
-                Optim.Options(iterations=10000,
-                            g_tol=1e-30,
-                            f_abstol=1e-30,
-                            f_reltol=1e-30,
-                            allow_f_increases=true,
-                            show_trace=false,
-                            callback=callback,
-                            time_limit = 60 * 60 * 24) # put a time limit of 1 day (units of seconds))
-                    )
-    push!(res_loop_minimum, res.minimum)
-    push!(res_loop_minimizer, res.minimizer)
-end
-minval,minloc = findmin(res_loop_minimum)
-minx0 = res_loop_minimizer[minloc]
+minval = res.minimum;
+minx0 = res.minimizer;
+stoppedby = res.stopped_by;
+optimiters = res.iterations;
 
 println("saving.")
 
-save("data/n$(n)_q$(q)_d$(d)_t$(t)/iter$(iter).jld2","minval",minval,"minx0",minx0,"n",n,"t",t,"d",d,"iter",iter,"q",q)
+save("data/n$(n)_q$(q)_d$(d)_t$(t)/iter$(iter).jld2","minval",minval,"minx0",minx0,"n",n,"t",t,"d",d,"iter",iter,"q",q,"stoppedby",stoppedby,"optimiters",optimiters)
 
 println("done.")
