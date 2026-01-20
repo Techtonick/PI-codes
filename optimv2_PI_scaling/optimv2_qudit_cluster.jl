8003da2edaeda67cf5627d5917dcb7a401bf2c38
Liam Bond <lblb98.lb98@gmail.com>
2026-01-20 15:08:15 +0100

    optimv2_qudit_cluster now with BigFloat

--- optimv2_PI_scaling/optimv2_qudit_cluster.jl
+++ optimv2_PI_scaling/optimv2_qudit_cluster.jl
@@ -26,7 +26,7 @@ const vμ = size(μ)[1];
 const vν = size(ν)[1];
 const binoms_diff_cached = binoms_diff_precompute(n, q, t, λ, μ, ν, vλ, vμ, vν);
 const nonneg_cached = nonneg_precompute(q, λ, μ, ν, vλ, vμ, vν);
-const optim_soltol = 1e-15;
+const optim_soltol = 1e-25;
 const pos_cached = partition_pos_precompute(n, q, d, t, λ, μ, ν, vλ, vμ, vν);
 
 #Define cost function
@@ -37,21 +37,18 @@ callback(state) = (abs(state.f_x) < optim_soltol ? (return true) : (return false
 const codeword_length = size(partitions_into_q_parts(n,q))[1];
 const num_var_params = d * codeword_length;
 
-res_loop_minimum = []; res_loop_minimizer = []; thread_arr = [];
-
-
-
 println("start optimization.")
 
-x0 = normalize(rand(num_var_params)) # this is for real 
+# x0 = normalize(rand(num_var_params)) # this is for real 
 # x0 = normalize(rand(ComplexF64,num_var_params)) # for Vlad: this is complex 
+x0 = normalize(rand(Complex{BigFloat},num_var_params)); # for Vlad: this is complex 
 
-res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), autodiff = AutoReverseDiff(),
-# res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), 
+# res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), autodiff = AutoReverseDiff(),
+res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), 
             Optim.Options(iterations=200000,
                         g_tol=1e-20,
-                        f_abstol=1e-20,
-                        f_reltol=1e-20,
+                        f_abstol=0,
+                        f_reltol=0,
                         allow_f_increases=true,
                         show_trace=false,
                         callback=callback,
@@ -60,12 +57,9 @@ res = optimize(cost, x0, LBFGS(linesearch=LineSearches.BackTracking()), autodiff
 
 minval = res.minimum;
 minx0 = res.minimizer;
-stoppedby = res.stopped_by;
+stoppedby = res.stopped_by
 optimiters = res.iterations;
 
-println("saving. minimum was $minval")
+println("saving. minimum was $minval. exit code was $([k for (k, v) in pairs(stoppedby) if v])")
 
-save("data/n$(n)_q$(q)_d$(d)_t$(t)/iter$(iter).jld2","minval",minval,"minx0",minx0,"n",n,"t",t,"d",d,"iter",iter,"q",q,"stoppedby",stoppedby,"optimiters",optimiters,"res",res)
-
-println("done.")
-    
+save("data/KLcomplex/n$(n)_q$(q)_d$(d)_t$(t)_complex$(!isreal(x0[1]))/iter$(iter).jld2","minval",minval,"minx0",minx0,"n",n,"t",t,"d",d,"iter",iter,"q",q,"stoppedby",stoppedby,"optimiters",optimiters,"res",res)
